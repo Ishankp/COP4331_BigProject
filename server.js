7f92e761345cce31af2ebdb9e95fa08f63f41cc9
Mistlo <96539755+Ishankp@users.noreply.github.com>
2024-10-28 18:45:26 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,206 @@
+
+const MongoClient = require('mongodb').MongoClient;
+
+// Correctly URL-encoded password
+const url = 'mongodb+srv://root:COP4331_RootP%40ssword@cluster0.wckml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const client = new MongoClient(url);
+
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
+
+var cardList =
+[
+	'Roy Campanella',
+	'Paul Molitor',
+	'Tony Gwynn',
+	'Dennis Eckersley',
+	'Reggie Jackson',
+	'Gaylord Perry',
+	'Buck Leonard',
+	'Rollie Fingers',
+	'Charlie Gehringer',
+	'Wade Boggs',
+	'Carl Hubbell',
+	'Dave Winfield',
+	'Jackie Robinson',
+	'Ken Griffey, Jr.',
+	'Al Simmons',
+	'Chuck Klein',
+	'Mel Ott',
+	'Mark McGwire',
+	'Nolan Ryan',
+	'Ralph Kiner',
+	'Yogi Berra',
+	'Goose Goslin',
+	'Greg Maddux',
+	'Frankie Frisch',
+	'Ernie Banks',
+	'Ozzie Smith',
+	'Hank Greenberg',
+	'Kirby Puckett',
+	'Bob Feller',
+	'Dizzy Dean',
+	'Joe Jackson',
+	'Sam Crawford',
+	'Barry Bonds',
+	'Duke Snider',
+	'George Sisler',
+	'Ed Walsh',
+	'Tom Seaver',
+	'Willie Stargell',
+	'Bob Gibson',
+	'Brooks Robinson',
+	'Steve Carlton',
+	'Joe Medwick',
+	'Nap Lajoie',
+	'Cal Ripken, Jr.',
+	'Mike Schmidt',
+	'Eddie Murray',
+	'Tris Speaker',
+	'Al Kaline',
+	'Sandy Koufax',
+	'Willie Keeler',
+	'Pete Rose',
+	'Robin Roberts',
+	'Eddie Collins',
+	'Lefty Gomez',
+	'Lefty Grove',
+	'Carl Yastrzemski',
+	'Frank Robinson',
+	'Juan Marichal',
+	'Warren Spahn',
+	'Pie Traynor',
+	'Roberto Clemente',
+	'Harmon Killebrew',
+	'Satchel Paige',
+	'Eddie Plank',
+	'Josh Gibson',
+	'Oscar Charleston',
+	'Mickey Mantle',
+	'Cool Papa Bell',
+	'Johnny Bench',
+	'Mickey Cochrane',
+	'Jimmie Foxx',
+	'Jim Palmer',
+	'Cy Young',
+	'Eddie Mathews',
+	'Honus Wagner',
+	'Paul Waner',
+	'Grover Alexander',
+	'Rod Carew',
+	'Joe DiMaggio',
+	'Joe Morgan',
+	'Stan Musial',
+	'Bill Terry',
+	'Rogers Hornsby',
+	'Lou Brock',
+	'Ted Williams',
+	'Bill Dickey',
+	'Christy Mathewson',
+	'Willie McCovey',
+	'Lou Gehrig',
+	'George Brett',
+	'Hank Aaron',
+	'Harry Heilmann',
+	'Walter Johnson',
+	'Roger Clemens',
+	'Ty Cobb',
+	'Whitey Ford',
+	'Willie Mays',
+	'Rickey Henderson',
+	'Babe Ruth'
+];
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use((req, res, next) =>
+{
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+	'GET, POST, PATCH, DELETE, OPTIONS'
+	);
+	next();
+});
+
+
+app.post('/api/addcard', async (req, res, next) =>
+{
+// incoming: userId, color
+// outgoing: error
+const { userId, card } = req.body;
+const newCard = {Card:card,UserId:userId};
+var error = '';
+try
+{
+const db = client.db('SchedulePlanner');
+const result = db.collection('Cards').insertOne(newCard);
+}
+catch(e)
+{
+error = e.toString();
+}
+cardList.push( card );
+var ret = { error: error };
+res.status(200).json(ret);
+});
+
+
+app.post('/api/login', async (req, res, next) =>
+{
+// incoming: login, password
+// outgoing: id, firstName, lastName, error
+var error = '';
+const { login, password } = req.body;
+const db = client.db('SchedulePlanner');
+const results = await
+db.collection('Users').find({Login:login,Password:password}).toArray();
+var id = -1;
+var fn = '';
+var ln = '';
+if( results.length > 0 )
+{
+id = results[0].UserId;
+fn = results[0].FirstName;
+ln = results[0].LastName;
+}
+var ret = { id:id, firstName:fn, lastName:ln, error:''};
+res.status(200).json(ret);
+});
+
+app.post('/api/searchcards', async (req, res, next) => {
+  // incoming: userId, search
+  // outgoing: results[], error
+  var error = '';
+  const { userId, search } = req.body;
+  var _search = search.trim();
+  try {
+      const db = client.db('SchedulePlanner');
+      const results = await db.collection('Cards').find({ "Card": { $regex: new RegExp(_search + '.*', 'i') } }).toArray();
+      var _ret = results.map(result => result.Card); // Simplified mapping
+      res.status(200).json({ results: _ret, error: error });
+  } catch (e) {
+      error = e.toString();
+      console.error('Error searching cards:', error);
+      res.status(500).json({ results: [], error });
+  }
+});
+
+
+app.listen(5000); // start Node + Express server on port 5000
+
